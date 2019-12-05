fbd5088d083643f9959950b24f6f59529de2f4db
Yang Song <ys3303@columbia.edu>
2019-12-05 23:36:09 +0000

    Change detail url

--- tracker/urls.py
+++ tracker/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('sightings/', views.all, name = "all"),
     path('sightings/add/',views.add, name = "add"),
     path('sightings/stats/',views.stats,name='stats'),
-    path('sightings/<Unique_Squirrel_ID>/', views.detail, name = "detail"),
-    path('sightings/<Unique_Squirrel_ID>/update/', views.update, name = "update"),
+    path('sightings/<Unique_Squirrel_ID>/', views.update, name = "update"),
     path('map/',views.map, name ='map'),
 ]
